f57c502009357f38fb7177ae98a0d65833cd7515
Mihai M <mmarinescu@192-168-0-102.rdsnet.ro>
2018-06-09 17:09:29 +0300

    Added form component communication

---
+++ src/components/Modal.js
@@ -0,0 +1,64 @@
+import React from 'react';
+
+export default class Modal extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            color: this.props.hexColor,
+            speed: this.props.speed,
+            error: false
+        };
+        this.handleSave = this.handleSave.bind(this);
+        this.handleColorChange = this.handleColorChange.bind(this);
+        this.handleSpeedChange = this.handleSpeedChange.bind(this);
+    }
+
+    handleSave(event) {
+        event.preventDefault();
+        const config = {
+            hexColor: this.state.color,
+            speed: this.state.speed
+        }
+        this.props.handleSubmit(config)
+        this.props.closeModal()
+    }
+    // handling input color change
+    handleColorChange(event) {
+        const regExp = /^#(?:[0-9a-f]{3}){1,2}$/i
+        const value = event.target.value
+        if (regExp.test(value)) {
+            this.setState({color: event.target.value});
+            this.setState({error: false})
+        } else {
+            this.setState({error: true})
+        }        
+    }
+    // handling input speed change
+    handleSpeedChange(event) {
+        this.setState({speed: event.target.value});
+    }
+    render() {
+        return(
+            <div className={`modal-container display-flex ${this.props.showModal ? '':'hide'} `}>
+                <div className="modal-header">
+                    <h2>Config</h2>
+                    <button onClick={this.props.closeModal}>x</button>
+                </div>
+                <div className="modal-body">
+                    <form name="configForm">
+                        <label className={this.state.error ? 'error': ''}>
+                            Color(Hex):
+                            <input type="text" name="color" placeholder={`ex. ${this.state.color}`} onChange={this.handleColorChange}/>
+                            <div className="error-message">Please enter a valid color</div>
+                        </label>
+                        <label>
+                            Speed(Seconds):
+                            <input type="number" name="speed" placeholder={`ex. ${this.state.speed}s`} onChange={this.handleSpeedChange}/>
+                        </label>
+                        <button onClick={this.handleSave}>Save</button>
+                    </form>
+                </div>
+            </div>
+        )
+    }
+}
---
+++ src/containers/App.js
@@ -0,0 +1,275 @@
+import React from 'react';
+import Modal from '../components/Modal';
+
+// Square is a presentation component - consists in only a render method
+function Square(props) {
+    return (
+        <div className='square' style={{backgroundColor: props.hexColor}}>
+        {props.value}
+        </div>
+    );
+}
+
+// Board is a presentation component that draws the board and passes the drawn squers as an array
+class Board extends React.Component {
+
+    render() {
+        const rows = [];
+        //  creating the board
+        for(let i = 0; i<49; i=i+7) {
+            const oneRow = [];
+            for(let j = i; j < i+7; j++) {
+                oneRow.push(this.renderSquare(j, j))
+            }
+            rows.push(<div className="board-row" key={i + 'board-row'}>{oneRow}</div>)
+        }
+        return (
+            <div>
+                {rows}
+            </div>
+        );
+    }
+
+    // renderSquare - accepts an index and a key
+    // the props.drawn returns a true/false value stored in the drawing array from the state of the app
+    renderSquare(i, key) {
+        return (
+            <Square
+                hexColor={this.props.drawn[i] ? this.props.hexColor : '#ffffff'}
+                key={key}
+                value={i}
+            />
+        );
+    }
+}
+
+
+// App is the Container component
+// Stores all the shared state between the popup and the board
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            drawing: Array(49).fill(null),
+            isDrawn: false,
+            showModal: false,
+            drawTime: 2,
+            hexColor: '#f4424b'
+        }
+        this.closeModal = this.closeModal.bind(this)
+        this.handleModal = this.handleModal.bind(this)
+    }
+
+    openModal() {
+        this.setState({
+            showModal: true
+        })
+    }
+
+    closeModal() {
+        this.setState({
+            showModal: false
+        })
+    }
+
+    drawSnake() {
+        // the current drawing in the state
+        let currentDrawing = this.state.drawing.slice()
+        currentDrawing = currentDrawing.fill(null, 0, 49)
+        this.setState({
+            drawing: currentDrawing
+        })
+        // the start of the snake drawing is random
+        const startIndex = getRandomInt(0, 48)
+        let isSnakeAlive = true
+        // array containing the positions on the board with the drawn snake
+        const snakeArray = []
+        // the last move will be the last correct move
+        let lastMove = startIndex
+        // snakeArray will contain all the correct moves
+        snakeArray.push(lastMove)
+        
+        // THIS IS SENT IN THE VIEW - adding the move in the state also
+        currentDrawing[lastMove] = true;
+        
+        // generating the "dead zones" arrays
+        const rightWall = generateDeadZoneRight(7)
+        const leftWall = generateDeadZoneLeft(7)
+        // drawing the snake while it's still alive
+        while (isSnakeAlive) {
+            const move = drawSnakeCell(lastMove)
+            // checkCell(move, historySnake, rightWall, leftWall)
+            const moveStatus = checkCell(move, snakeArray, rightWall, leftWall)
+            if (moveStatus === 'dead') {
+                isSnakeAlive = false;
+                this.setState({
+                    isDrawn: true
+                })
+            } else {
+                lastMove = move.pos
+                snakeArray.push(lastMove)
+                currentDrawing[lastMove] = true;
+                const prevState = this.state
+                // updating the array sent to the view
+                this.setState({
+                    ...prevState,
+                    drawing: currentDrawing
+                })
+            }
+        } 
+    }
+
+    // this will be called from the modal component with passed data
+    // @param: {hexColor: String, speed: Number}
+    handleModal(passed) {
+        const prevState = this.state
+        this.setState({
+            ...prevState,
+            hexColor: passed.hexColor,
+            speed: passed.speed
+        })
+    }
+
+    render() {
+        const btnText = this.state.isDrawn ? 'Redraw' : 'Draw'
+        return (
+            <div className="app disp-flex">
+                <div className="app-header disp-flex">
+                    <div className="logo">LOGO</div>
+                    <div className="config">
+                        <div>
+                            <button onClick={() => this.openModal()}>Config {this.props.seconds}</button>
+                        </div>
+                        
+                    </div>
+                </div>
+                <div>
+                    <div className="app-board disp-flex">
+                        <Board drawn={this.state.drawing}
+                                hexColor={this.state.hexColor}
+                        />
+                    </div>
+                    <div className="app-modal disp-flex">
+                        <Modal showModal={this.state.showModal} 
+                                closeModal={this.closeModal} 
+                                handleSubmit={this.handleModal}
+                                speed={this.state.drawTime}
+                                hexColor={this.state.hexColor}
+                        />
+                    </div>
+                    <div className="draw-btn-container disp-flex row-center">
+                        <button onClick={() => this.drawSnake()}>{btnText}</button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+
+// Helper functions
+// Function for just drawing one cell
+function drawSnakeCell(index) {
+    function add1(index) {
+        // moving snake head right
+        return {
+            pos: index+1,
+            dir: "right"
+        }
+    }
+    function minus1(index) {
+        //moving snake head left
+        return {
+            pos: index-1,
+            dir: "left"
+        }
+    }
+    function add7(index) {
+        //moving head snake down
+        return {
+            pos: index+7,
+            dir: "down"
+        }
+    }
+    function minus7(index) {
+        //moving head snake up
+        return {
+            pos: index-7,
+            dir: "up"
+        }
+    }
+    // registering one of the four moves
+    const availableMoves = [add1, minus1, add7, minus7]
+    // generating a random move out of the fore
+    const randomMove = getRandomInt(0, 3)
+
+    // the drawing of the cell is random
+    const nextMoveValue = availableMoves[randomMove](index)
+    
+    return nextMoveValue;
+}
+
+/*
+Understanding the board - the lines are walls up<0, down>48, 
+rightWall - going from 6 to 7, left wall - going from 14 to 13 in the array
+_____________________
+| 0  1  2  3  4  5  6 |
+| 7  8  9 10 11 12 13 |
+|14 15 16 17 18 19 20 |
+|21 22 23 24 25 26 27 |
+|28 29 30 31 32 33 34 |
+|35 36 37 38 39 40 41 |
+|42 43 44 45 46 47 48 |
+_____________________
+*/
+
+function checkCell(move, historySnake, rightWall, leftWall) {
+    /* // checking if the snake went into itself - commented out since it does this often
+    if(historySnake.includes(move.pos)) {        
+        return 'dead'
+    } else {*/
+        // checking if the snake went into the walls (aka, deadzones)
+        switch(move.dir) {
+            case 'up':
+                return move.pos < 0 ? 'dead' : move.pos
+            case 'down': 
+                return move.pos > 48 ? 'dead' : move.pos
+            case 'right':
+                return rightWall.includes(move.pos) ? 'dead' : move.pos
+            case 'left':
+                return leftWall.includes(move.pos) ? 'dead' : move.pos
+            default:
+                return 'dead'
+        }
+    // }    
+}
+
+/**
+ * Returns a random number between min (inclusive) and max (exclusive)
+ */
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+function generateDeadZoneRight(rows) {
+    // this array is used to check if the snake move right into the wall
+    const arr = []
+    let deadIndex = 0;
+    for(let i=0; i<rows;i++) {
+        arr.push(deadIndex+7)
+        deadIndex+=7;
+    }
+    return arr;
+}
+
+function generateDeadZoneLeft(rows) {
+    // this array is used to check if the snake move left into the wall
+    const arr = []
+    let deadIndex = 0;
+    for(let i=0; i<rows;i++) {
+        arr.push(deadIndex+6)
+        deadIndex+=7;
+    }
+    return arr;
+}
